d543a2fae64252c0c1f8087f7042d398e15b6fcb
Dmitry Yakubov <dmitry.yakubov@gmail.com>
2017-05-10 13:05:35 +0500

    change var from $_GET to $_POST

--- w1.php
+++ w1.php
@@ -3,12 +3,12 @@ $filename1 = "log.txt";
 
 $fp = fopen( $filename1, "a" );
 
-foreach ($_GET as $key => $val)
+foreach ($_POST as $key => $val)
 {
     fwrite($fp, $key.' => '.$val."\r\nFWRITE");
 }
 
-echo "<pre>".print_r($_GET)."</pre>";
+echo "<pre>".print_r($_POST)."</pre>";
 fclose($fp);
 
 ?>
