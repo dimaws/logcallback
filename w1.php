daaad063c33fc609913c1835e365b107cad61149
Dmitry Yakubov <dmitry.yakubov@gmail.com>
2017-05-10 13:12:14 +0500

    WMI_RESULT=OK

--- w1.php
+++ w1.php
@@ -5,10 +5,13 @@ $fp = fopen( $filename1, "a" );
 
 foreach ($_POST as $key => $val)
 {
-    fwrite($fp, $key.' => '.$val."\r\nFWRITE");
+    fwrite($fp, $key.' => '.$val."\r\n");
 }
 
-echo "<pre>".print_r($_POST)."</pre>";
+echo "WMI_RESULT=OK";
+//echo "<pre>".print_r($_POST)."</pre>";
 fclose($fp);
 
+echo "WMI_RESULT=OK";
+//echo "<pre>".print_r($_POST)."</pre>";
 ?>
